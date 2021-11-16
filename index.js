5e1fc57705d55eda19755c0fd2776aec16eb347a
Marcelo Luiz <mlluizpereira39@gmail.com>
2021-11-16 12:43:04 -0300

    chore: add to port

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
 })
